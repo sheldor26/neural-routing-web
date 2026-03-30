bd9cefe6dce7e68c8a30d6e85fab739f8fb6cc45
sheldor26 <juanmirande10@gmail.com>
2026-03-30 15:56:35 -0300

    Feat: Connect Supabase and update Dashboard with real-time tracking

---
+++ lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// Este es el cliente que usaremos en el Frontend
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
